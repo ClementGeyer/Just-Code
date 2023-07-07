f32c6f8e380aa9ac6fa5feefd8710aadd4a80842
Clément Geyer <clementgeyer4@gmail.com>
2023-07-07 03:55:06 +0200

    get next line text

--- justcode/src/commands.ts
+++ justcode/src/commands.ts
@@ -24,16 +24,22 @@ export const insertInlineCommentCommand = () => {
         // Select the highlighted text
         const selection = activeEditor.selection
 
+        // Text selected or not
         if (selection && !selection.isEmpty) {
             const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
             const highlighted = activeEditor.document.getText(selectionRange);
 
-            let data = await generateComment(highlighted);
-            console.log(data)
+            // let data = await generateComment(highlighted);
 
             activeEditor.edit(editBuilder => {
-                editBuilder.insert(activeEditor.selection.active, data.content)
+                editBuilder.insert(activeEditor.selection.active, "data.content")
             })
+        } else {
+          //get next line text and remove spaces
+          const nextLine = activeEditor.document.lineAt(selection.active.line + 1).text;
+          if(nextLine){
+            console.log(nextLine.trim())
+          }
         }
     });
 }
