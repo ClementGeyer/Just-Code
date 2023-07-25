49226f49d9fdef3b02bdac47f6ec1ec8a8a8ff51
Clément Geyer <clementgeyer4@gmail.com>
2023-07-25 21:26:34 +0200

    handle ReactJS nested elements

--- justcode/src/commands.ts
+++ justcode/src/commands.ts
@@ -36,32 +36,35 @@ export async function insertDocstringComment() {
 
   let docstring = '';
   let functionRange: vscode.Range | undefined;
+  let functionName: string | undefined;
+  //const language = editor.document.languageId;;
 
-  // Non-nested files
+  // Non-nested files get right function, ReactJS get JSX main element
   traverse(ast, {
     FunctionDeclaration(path) {
       const node = path.node as t.FunctionDeclaration;
       const { start, end } = node.loc!;
       if (position >= start.line && position <= end.line) {
-        const functionName = node.id?.name || 'unnamedFunction';
+        functionName = node.id?.name || 'unnamedFunction';
         docstring = `/**\n * Function ${functionName}\n * @param {void} paramName - desc\n * @returns {void} - desc\n */`;
         functionRange = new vscode.Range(
           new vscode.Position(start.line - 1, 0), // Adjust for 0-indexing
           new vscode.Position(end.line, 0)
         );
-        path.stop(); // Stop traversing the AST as we found the enclosing function.
+        path.stop();
       }
     },
     ArrowFunctionExpression(path) {
       const node = path.node as t.ArrowFunctionExpression;
       const { loc } = node;
       if (loc && loc.start.line <= position && loc.end.line >= position) {
+        //TODO: make a docstring better
         docstring = `/**\n * Arrow Function\n * @returns {void}\n */`;
         functionRange = new vscode.Range(
           new vscode.Position(loc.start.line - 1, 0), // Adjust for 0-indexing
           new vscode.Position(loc.end.line, 0)
         );
-        path.stop(); // Stop traversing the AST as we found the enclosing arrow function.
+        path.stop();
       }
     },
   });
@@ -72,25 +75,30 @@ export async function insertDocstringComment() {
   }
 
   if (functionRange) {
-    // Get the text of the function
-    const lastChild = findLastChildFunction(ast, editor.selection.active)
     let lastChildText = "";
-    console.log('lastchild', lastChild)
+    let functionText = ""
+    let docStringComment: any;
+
+    // Handles ReactJS files, even if not .jsx
+    const lastChild = findLastChildFunction(ast, editor.selection.active)
 
     // Perform custom selection to highlight the function range
     if(lastChild) {
       let lastChildRange = new vscode.Selection(lastChild?.start, lastChild?.end);
       editor.selection = lastChildRange
       lastChildText = editor.document.getText(lastChildRange);
+      docStringComment = await generateJSDocstringComment(lastChildText);
+    } else {
+      let range = new vscode.Selection(functionRange?.start, functionRange?.end)
+      editor.selection = range
+      functionText = editor.document.getText(functionRange);
+      docStringComment = await generateJSDocstringComment(functionText);
     }
 
-    const docStringComment = await generateJSDocstringComment(lastChildText);
-
     editor.edit((editBuilder) => {
-      // TODO: Adapt it to arrow functions
       const cursorPosition = editor.selection.start
+      // declaration line broken in jsx files
       const declarationLine = getFunctionDeclarationLine(ast, cursorPosition);
-      console.log(declarationLine)
       if(declarationLine)
         editBuilder.insert(new vscode.Position(declarationLine - 1, 0), docStringComment.content + '\n\n');
     }); 
@@ -100,7 +108,6 @@ export async function insertDocstringComment() {
 
 // Rework this (might only need if non-nested file)
 function getFunctionDeclarationLine(ast: t.File, position: vscode.Position): number | undefined {
-  console.log(position.line)
 
   let lineNumber: number | undefined;
 
@@ -133,26 +140,21 @@ function findLastChildFunction(node: t.Node, position: vscode.Position): { start
   traverse(node, {
     FunctionDeclaration(path) {
       const fnNode = path.node as t.FunctionDeclaration;
-      console.log('func')
       if (fnNode.loc && fnNode.loc.start.line <= position.line && fnNode.loc.end.line >= position.line) {
-        console.log(fnNode.loc.start.line, fnNode.loc.end.line)
         innermostFunction = {
           start: new vscode.Position(fnNode.loc.start.line - 1, 0), // Adjust for 0-indexing
           end: new vscode.Position(fnNode.loc.end.line, 0),
         };
       }
     },
-    //TODO: nested files arrows functions not handled
     ArrowFunctionExpression(path) {
       const arrowNode = path.node as t.ArrowFunctionExpression;
+      //Maybe handle column positioning
       if (
         arrowNode.loc &&
         arrowNode.loc.start.line <= position.line &&
-        arrowNode.loc.start.column <= position.character &&
-        arrowNode.loc.end.line >= position.line &&
-        arrowNode.loc.end.column >= position.character
+        arrowNode.loc.end.line >= position.line 
       ) {
-        console.log('loc', arrowNode)
         innermostFunction = {
           start: new vscode.Position(arrowNode.loc.start.line - 1, 0), // Adjust for 0-indexing
           end: new vscode.Position(arrowNode.loc.end.line, 0),
